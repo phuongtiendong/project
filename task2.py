0669f108a4aaa0f34b1bb22e5cd3bfcf74689c64
phuongtiendong <83067798+phuongtiendong@users.noreply.github.com>
2022-03-21 00:42:28 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,7 @@
+#Author: TienDong2k and Vuong Truong Son
+#Date : 20-03-2021
+def f2():
+  sec = int(input("Enter seconds: "))
+  m, s = divmod(sec, 60)
+  h, m = divmod(m, 60)
+  return "{:02d}:{:02d}:{:02d}".format(h, m, s)
---
+++ task3.py
@@ -0,0 +1,7 @@
+#Hello
+def f3():
+  n = input("Ввод n: ")
+  nn = n + n
+  nnn = n + n + n
+  ans = int(n) + int(nn) + int(nnn)
+  return n + " + " + nn + " + " + nnn + " = " + str(ans)
